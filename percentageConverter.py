8040a280666ca312d902a92f0d9ba7ca7a8d175d
Samuel Forrest <Samuel Forrest>
2016-12-11 22:28:58 +0000

    Added base percentageConvertor, with all initialisation code done.

---
+++ percentageConverter.py
@@ -0,0 +1,10 @@
+#PercentageConverter
+#Tkinter app to convert percentages with
+
+from Tkinter import *
+
+class porcentaje(Frame):
+
+    def __init__(self, master):
+        super(self, porcentaje).__init__(master)
+        
